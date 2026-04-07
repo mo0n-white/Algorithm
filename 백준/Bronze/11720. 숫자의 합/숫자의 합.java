c147a9ff750c73a017efa3d6be039f6a4ceca3c9
hyerin <89730398+mo0n-white@users.noreply.github.com>
2026-04-07 22:45:29 +0900

    [comments] pseudo code
    Added comments to improve code readability.

--- 백준/Bronze/11720. 숫자의 합/숫자의 합.java
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.java
@@ -3,15 +3,15 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int count = sc.nextInt();
+        int count = sc.nextInt(); // N값 입력
         int[] arr = new int[count];
         int total = 0;
 
-        String numList = sc.next();
+        String numList = sc.next(); // N자리 수 String으로 저장
 
         for (int i = 0; i < count; i++) {
-            arr[i] = numList.charAt(i) - '0';
-            total += arr[i];
+            arr[i] = numList.charAt(i) - '0'; // 각 자리수를 char형으로 가져온 후 '0'을 빼서 int로 형 변환
+            total += arr[i]; // 누적합
         }
 
         System.out.println(total);
