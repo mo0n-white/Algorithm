2614c6708a2d7a8fb083fb664c7b898cfb82dc7b
hyerin <89730398+mo0n-white@users.noreply.github.com>
2026-04-07 23:08:55 +0900

    [Bronze I] Title: 평균, Time: 216 ms, Memory: 18852 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,22 +1,26 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+        int count = sc.nextInt(); // N 입력
+        int[] arr = new int[count];
+        int max = 0;
+        int total = 0;
+        double avg = 0.0;
 
-        int subCount = scanner.nextInt();
-        int[] score = new int[subCount];
-        int maxScore = 0;
-        int sum = 0;
-        double newAvg = 0.0;
-
-        for(int i=0; i<subCount; i++) {
-            score[i] = scanner.nextInt();
-            sum += score[i];
-            maxScore = Math.max(maxScore, score[i]);
+        for (int i = 0; i < count; i++) {// 점수 입력(정수 배열) + 최댓값 탐색 + 총합 계산(평균 계산을 위해)
+            arr[i] = sc.nextInt();
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+            total += arr[i];
         }
+        
+        avg = ((double) total / count) / max * 100; // 수정 후 평균 = (평균/최댓값) * 100
+        
+        System.out.println(avg);
 
-        newAvg = ((double)sum/(double)subCount)/(double)maxScore*100;
-        System.out.print(newAvg);
+        sc.close();
     }
 }
