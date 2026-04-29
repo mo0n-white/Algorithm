47f782a49b7864bb1ac9b3714c64f306cc5954f9
hyerin <89730398+mo0n-white@users.noreply.github.com>
2026-04-29 16:44:00 +0900

    refactor w.GPT

--- 백준/Gold/1253. 좋은 수/좋은 수_book.java
+++ 백준/Gold/1253. 좋은 수/좋은 수_book.java
@@ -23,30 +23,33 @@ public class Main {
         long add;
         int count = 0;
         
-        for (int i=0; i<N; i++) {   // 투 포인터; for i 0~N
-            if(N == 1 || N == 2) break;
+        if(N == 1 || N == 2) {
+            System.out.println(0);
+            return;
+        }
 
+        for (int i=0; i<N; i++) {   // 투 포인터; for i 0~N
             start =  0;
             end = N-1;
-
-            if(start == i) start++;
-            if(end == i) end--;
-
-            add = numArr[start] + numArr[end]; // 런타임 에러 N이 1인 경우, start와 end가 1이 되어버림
 
             long target = numArr[i];
 
             while(start < end) {
+                if(start == i) {
+                    start++;
+                    continue;
+                }
+                if(end == i) {
+                    end--;
+                    continue;
+                }
+
+                add = numArr[start] + numArr[end];
+
                 if (add < target) {         // add < target : start++; add = start + end; 
                     start++;
-                    if(start == i) start++;
-                    if(start >= end) break; // 런타임에러 방지
-                    add = numArr[start] + numArr[end];
                 } else if (add > target) {  // add > target : end--; add = start + end;
                     end--;
-                    if(end == i) end--;
-                    if(start >= end) break; // 런타임에러 방지
-                    add = numArr[start] + numArr[end];
                 } else {                    // add == target : count++;
                     count++;
                     break;
